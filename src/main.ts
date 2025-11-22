2af985073457721a6188853b6dc0fd9a137326ed
ChoiDevv <csd_1996@naver.com>
2025-11-22 16:00:32 +0900

    nest initialized

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    logger: false,
+  });
+  await app.listen(Number(process.env.PORT));
+}
+bootstrap();
